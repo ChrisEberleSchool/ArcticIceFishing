b861fcd658f4375b38e87813fda361d13aae6828
ChrisEberleSchool <ceber042@mtroyal.ca>
2025-08-05 17:20:18 -0600

    sd

--- network/socket.js
+++ network/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
 // This makes the socket connection reusable across all scenes/modules
-//const socket = io("https://arcticicefishing-1.onrender.com");
-const socket = io("http://localhost:3000");
+const socket = io("https://arcticicefishing-1.onrender.com");
 
 export default socket;
