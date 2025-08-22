a63080321cf4fbafdb273d268abd3ddadea07685
ChrisEberleSchool <ceber042@mtroyal.ca>
2025-08-22 16:21:15 -0600

    partially added shop ui

--- client/network/socket.js
+++ client/network/socket.js
@@ -1,12 +1,11 @@
 import { io } from "socket.io-client";
 
-const baseURL =
-  window.location.hostname === "localhost"
+const socket = io(
+  process.env.NODE_ENV === "production"
+    ? "https://arcticicefishing.onrender.com"
+    : window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : window.location.origin; // automatically uses https://yourdomain.com
-
-const socket = io(baseURL, {
-  transports: ["websocket"], // ensures secure websocket connection
-});
+    : "https://arcticicefishing.onrender.com"
+);
 
 export default socket;
